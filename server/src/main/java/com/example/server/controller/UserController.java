c33a7885349943f39f6aed4d9fb7cc1b4424a04b
FirePancake1177 <poiskov2016@gmail.com>
2024-03-15 20:16:56 +0300

    add PUT request endpoint for changing User Info

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.server.controller;
 
 import com.example.server.dto.UserLoginDto;
 import com.example.server.dto.UserRegistrationDto;
+import com.example.server.dto.UserUpdateDto;
 import com.example.server.model.User;
 import com.example.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ public class UserController {
     public User registerUser(@RequestBody UserRegistrationDto registrationDto) {
         return userService.registerUser(registrationDto);
     }
+
     // example of usage:
     // curl -i -X POST -H "Content-Type: application/json" -d '{"username":"testuser", "password":"testpassword"}' http://localhost:8080/login
     @PostMapping("/login")
@@ -35,5 +37,27 @@ public class UserController {
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+
+    //curl -X PUT -H "Content-Type: application/json" \
+    //-d '{
+    //    "username": "newUsername",
+    //    "email": "newEmail@example.com",
+    //    "password": "newPassword",
+    //    "profilePictureUrl": "newProfilePictureUrl",
+    //    "firstName": "newFirstName",
+    //    "secondName": "newSecondName",
+    //    "dateOfBirth": "1990-01-01",
+    //    "country": "newCountry",
+    //    "city": "newCity",
+    //    "education": "newEducation",
+    //    "bio": "newBio",
+    //    "resumeUrl": "newResumeUrl"
+    //}' \
+    //http://localhost:8080/users/1
+    //                            ^ user id here
+    @PutMapping("/users/{userId}")
+    public User updateUser(@PathVariable Integer userId, @RequestBody UserUpdateDto updateDto) {
+        return userService.updateUser(userId, updateDto);
     }
 }
