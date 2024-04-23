3c42850f95713a13ae69bfd55d3f8ac7ae04b5a2
m4xxx1m <mi4469228@yandex.ru>
2024-04-24 01:57:31 +0300

    Implemented viewing and sending comments

--- client/composeApp/src/commonMain/kotlin/navigation/CommentScreen.kt
+++ client/composeApp/src/commonMain/kotlin/navigation/CommentScreen.kt
@@ -1,28 +1,45 @@
 package navigation
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import kotlinx.coroutines.launch
+import model.AuthManager
 import model.Comment
 import model.Post
 import model.User
 import network.ApiInterface
+import network.CommentCreate
 import network.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -31,104 +48,181 @@ import ui.CommentCard
 import ui.PostCard
 
 class CommentScreen(private val postId: Int) : Screen {
+    private var commentText: MutableState<String>? = null
+
     @Composable
     override fun Content() {
-        val refreshHelper = remember { mutableStateOf(RefreshCommentsHelper(postId)) }
-        RefreshableContent(refreshHelper) {
-            Column(modifier = Modifier.fillMaxSize().padding(horizontal = 15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                refreshHelper.value.post.value?.let {
-                    PostCard(it)
-                }
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize().padding(horizontal = 15.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    contentPadding = PaddingValues(
-                        top = 10.dp,
-                        bottom = 10.dp
-                    )
+        commentText = remember { mutableStateOf("") }
+        val refreshHelper = remember { mutableStateOf(RefreshCommentsHelper()) }
+        val coroutineScope = rememberCoroutineScope()
+        Scaffold(
+            topBar = {
+                BackButton(LocalNavigator.currentOrThrow)
+            },
+            bottomBar = {
+                Box(
+                    modifier = Modifier.fillMaxWidth().background(Color.White).padding(7.dp),
+                    contentAlignment = Alignment.BottomCenter
                 ) {
-                    items(refreshHelper.value.comments) { comment ->
-                        comment.user = refreshHelper.value.users[comment.authorId]
-                        CommentCard(comment)
-                        Spacer(Modifier.size(10.dp))
+                    Row(
+                        modifier = Modifier.widthIn(max = 500.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        OutlinedTextField(
+                            modifier = Modifier.weight(1f),
+                            value = commentText?.value ?: "",
+                            onValueChange = {
+                                commentText?.value = it
+                            },
+                            label = {
+                                Text("Comment")
+                            }
+                        )
+                        IconButton(
+                            onClick = {
+                                coroutineScope.launch {
+                                    refreshHelper.value.sendComment()
+                                }
+                            },
+                            enabled = commentText?.value?.isNotEmpty() ?: false
+                        ) {
+                            Image(Icons.Rounded.Send, contentDescription = "Send comment")
+                        }
+                    }
+                }
+            }
+        ) { innerPadding ->
+            Box(
+                Modifier.padding(
+                    bottom = innerPadding.calculateBottomPadding(),
+                    top = innerPadding.calculateTopPadding()
+                )
+            ) {
+                RefreshableContent(refreshHelper) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize().padding(horizontal = 15.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        contentPadding = PaddingValues(
+                            top = 10.dp,
+                            bottom = 10.dp
+                        )
+                    ) {
+                        item {
+                            refreshHelper.value.post.value?.let { post ->
+                                post.user = refreshHelper.value.users[post.userId]
+                                PostCard(post, true)
+                                Spacer(Modifier.size(15.dp))
+                            }
+                        }
+                        items(refreshHelper.value.comments) { comment ->
+                            comment.user = refreshHelper.value.users[comment.authorId]
+                            CommentCard(comment)
+                            Spacer(Modifier.size(10.dp))
+                        }
                     }
                 }
             }
         }
-        BackButton(LocalNavigator.currentOrThrow)
     }
-}
 
-private class RefreshCommentsHelper(private val postId: Int) : Refreshable() {
-    val post = mutableStateOf<Post?>(null)
-    val comments = mutableStateListOf<Comment>()
-    val users = mutableStateMapOf<Int, User>()
+    inner class RefreshCommentsHelper : Refreshable() {
+        val post = mutableStateOf<Post?>(null)
+        val comments = mutableStateListOf<Comment>()
+        val users = mutableStateMapOf<Int, User>()
+        val retrofitCall: ApiInterface = RetrofitClient.retrofit.create(ApiInterface::class.java)
 
-    override fun load() {
-        val retrofitCall = RetrofitClient.retrofit.create(ApiInterface::class.java)
-
-        retrofitCall.getComments(postId).enqueue(object : Callback<List<network.Comment>> {
-            override fun onFailure(call: Call<List<network.Comment>>, t: Throwable) {
-                println("refresh comments screen failure")
-                isRefreshing = false
+        fun sendComment() {
+            if (commentText == null) {
+                return
             }
-
-            override fun onResponse(
-                call: Call<List<network.Comment>>,
-                response: Response<List<network.Comment>>
-            ) {
-                if (response.code() == 200) {
-                    println("refresh comments screen success")
-                    comments.clear()
-                    val userIds = mutableSetOf<Int>()
-                    response.body()?.let {
-                        comments.addAll(it.reversed().map { comment ->
-                            userIds.add(comment.authorId)
-                            comment.convertComment()
-                        })
+            retrofitCall.addComment(
+                postId,
+                CommentCreate(AuthManager.currentUser.id, commentText?.value ?: "")
+            ).enqueue(object : Callback<network.Comment> {
+                override fun onResponse(
+                    call: Call<network.Comment>,
+                    response: Response<network.Comment>
+                ) {
+                    if (response.code() == 200) {
+                        commentText?.value = ""
+                        load()
+                    } else {
+                        println("sending comment wrong code")
                     }
+                }
 
-                    retrofitCall.getPost(postId).enqueue(object : Callback<network.Post> {
-                        override fun onFailure(call: Call<network.Post>, t: Throwable) {
-                            println("refresh comments screen failure")
+                override fun onFailure(call: Call<network.Comment>, t: Throwable) {
+                    println("sending comment failure")
+                }
+            })
+        }
+
+        override fun load() {
+            retrofitCall.getComments(postId).enqueue(object : Callback<List<network.Comment>> {
+                override fun onFailure(call: Call<List<network.Comment>>, t: Throwable) {
+                    println("refresh comments screen failure")
+                    isRefreshing = false
+                }
+
+                override fun onResponse(
+                    call: Call<List<network.Comment>>,
+                    response: Response<List<network.Comment>>
+                ) {
+                    if (response.code() == 200) {
+                        println("refresh comments screen success")
+                        comments.clear()
+                        val userIds = mutableSetOf<Int>()
+                        response.body()?.let {
+                            comments.addAll(it.map { comment ->
+                                userIds.add(comment.authorId)
+                                comment.convertComment()
+                            })
                         }
 
-                        override fun onResponse(
-                            call: Call<network.Post>,
-                            response: Response<network.Post>
-                        ) {
-                            response.body()?.let {
-                                post.value = it.convertPost()
+                        retrofitCall.getPost(postId).enqueue(object : Callback<network.Post> {
+                            override fun onFailure(call: Call<network.Post>, t: Throwable) {
+                                println("refresh comments screen failure")
                             }
 
-                            retrofitCall.getUsersList(userIds)
-                                .enqueue(object : Callback<List<network.User>> {
-                                    override fun onFailure(
-                                        call: Call<List<network.User>>,
-                                        t: Throwable
-                                    ) {
-                                        println("get users list failure")
-                                    }
+                            override fun onResponse(
+                                call: Call<network.Post>,
+                                response: Response<network.Post>
+                            ) {
+                                response.body()?.let {
+                                    post.value = it.convertPost()
+                                }
+                                post.value?.let {
+                                    userIds.add(it.userId)
+                                }
 
-                                    override fun onResponse(
-                                        call: Call<List<network.User>>,
-                                        response: Response<List<network.User>>
-                                    ) {
-                                        response.body()?.let {
-                                            it.forEach { user ->
-                                                users[user.userId] = user.convertUser()
+                                retrofitCall.getUsersList(userIds)
+                                    .enqueue(object : Callback<List<network.User>> {
+                                        override fun onFailure(
+                                            call: Call<List<network.User>>,
+                                            t: Throwable
+                                        ) {
+                                            println("get users list failure")
+                                        }
+
+                                        override fun onResponse(
+                                            call: Call<List<network.User>>,
+                                            response: Response<List<network.User>>
+                                        ) {
+                                            response.body()?.let {
+                                                it.forEach { user ->
+                                                    users[user.userId] = user.convertUser()
+                                                }
                                             }
                                         }
-                                    }
-                                })
-                        }
-                    })
-                } else {
-                    println("refresh comments screen wrong code")
+                                    })
+                            }
+                        })
+                    } else {
+                        println("refresh comments screen wrong code")
+                    }
+                    isRefreshing = false
                 }
-                isRefreshing = false
-            }
-        })
+            })
+        }
     }
 }
