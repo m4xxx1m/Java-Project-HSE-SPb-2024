fe680f2ee4540a9d69a8017494ece8078dffb9a8
m4xxx1m <mi4469228@yandex.ru>
2024-04-24 01:57:31 +0300

    Implemented viewing and sending comments

--- client/composeApp/src/commonMain/kotlin/ui/CommentCard.kt
+++ client/composeApp/src/commonMain/kotlin/ui/CommentCard.kt
@@ -3,13 +3,14 @@ package ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
@@ -31,60 +32,59 @@ import model.Comment
 
 @Composable
 fun CommentCard(comment: Comment, isAnswerToAnswer: Boolean = false) {
-    Card(elevation = 6.dp, modifier = Modifier.fillMaxWidth()) {
-        Box(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
-            Row() {
-                Image(Icons.Rounded.Person, contentDescription = null,
-                    modifier = Modifier.size(35.dp).clip(CircleShape).background(Color.Red)
-                        .clickable { })
-                Spacer(Modifier.size(10.dp))
-                Column() {
-                    Row() {
-                        Column(Modifier.weight(1f)) {
-                            Text(comment.user?.name ?: "", fontSize = 18.sp)
-                            Text(comment.text, fontSize = 14.sp)
-                        }
-                        Column() {
+    Card(elevation = 6.dp, modifier = Modifier.widthIn(max = 500.dp).fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+            Spacer(Modifier.width(35.dp))
+            Image(Icons.Rounded.Person, contentDescription = null,
+                modifier = Modifier.size(35.dp).clip(CircleShape).background(Color.Red)
+                    .clickable { })
+            Spacer(Modifier.size(10.dp))
+            Column() {
+                Row() {
+                    Column(Modifier.weight(1f)) {
+                        Text(comment.user?.name ?: "", fontSize = 18.sp)
+                        Text(comment.text, fontSize = 14.sp)
+                    }
+                    Column() {
+                        Image(
+                            Icons.Rounded.MoreVert,
+                            contentDescription = "Show dropdown menu",
+                            modifier = Modifier.size(31.dp).clip(CircleShape).clickable { }
+                                .padding(3.dp)
+                        )
+                        if (isAnswerToAnswer) {
                             Image(
-                                Icons.Rounded.MoreVert,
-                                contentDescription = "Show dropdown menu",
+                                Icons.Rounded.KeyboardArrowUp,
+                                contentDescription = "To previous comment",
                                 modifier = Modifier.size(31.dp).clip(CircleShape).clickable { }
                                     .padding(3.dp)
                             )
-                            if (isAnswerToAnswer) {
-                                Image(
-                                    Icons.Rounded.KeyboardArrowUp,
-                                    contentDescription = "To previous comment",
-                                    modifier = Modifier.size(31.dp).clip(CircleShape).clickable { }
-                                        .padding(3.dp)
-                                )
-                            }
                         }
                     }
-                    LazyRow {
+                }
+                LazyRow {
 
-                    }
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(comment.dateTime.toString(), color = Color.LightGray, fontSize = 11.sp)
-                        Spacer(Modifier.weight(1f))
-                        Image(Icons.Rounded.MailOutline, contentDescription = "Answer",
-                            modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
-                                .clickable { }
-                        )
-                        Image(
-                            Icons.Rounded.KeyboardArrowUp,
-                            contentDescription = "Upvote comment",
-                            modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
-                                .clickable { }
-                        )
-                        Text(comment.likesCount.toString())
-                        Image(
-                            Icons.Rounded.KeyboardArrowDown,
-                            contentDescription = "Downvote comment",
-                            modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
-                                .clickable { }
-                        )
-                    }
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(comment.dateTime.toString(), color = Color.LightGray, fontSize = 11.sp)
+                    Spacer(Modifier.weight(1f))
+                    Image(Icons.Rounded.MailOutline, contentDescription = "Answer",
+                        modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
+                            .clickable { }
+                    )
+                    Image(
+                        Icons.Rounded.KeyboardArrowUp,
+                        contentDescription = "Upvote comment",
+                        modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
+                            .clickable { }
+                    )
+                    Text(comment.likesCount.toString())
+                    Image(
+                        Icons.Rounded.KeyboardArrowDown,
+                        contentDescription = "Downvote comment",
+                        modifier = Modifier.size(31.dp).padding(3.dp).clip(CircleShape)
+                            .clickable { }
+                    )
                 }
             }
         }
