dd7f190e8d7146fd71b22f94e07901a5a9f568ba
Alisa Strazdina <73630127+shymaki@users.noreply.github.com>
2024-05-08 11:46:49 +0300

    Change tags logic; add tags for user

--- server/src/main/java/com/example/server/model/Tag.java
+++ server/src/main/java/com/example/server/model/Tag.java
@@ -1,26 +1,56 @@
 package com.example.server.model;
 
-import jakarta.persistence.*;
+import java.util.*;
 
-@Entity
-@Table(name = "tags")
 public class Tag {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int tagId;
-    @Column(length = 50)
-    private final String tagName;
+    private static final List<String> tagNames = new ArrayList<>(Arrays.asList("C++", "Python", "Java",
+            "JavaScript", "Go", "Yandex",
+            "Tinkoff", "SberSeasons"));
 
-    public Tag(String tagName) {
-        this.tagName = tagName;
+    private static final Map<String, Integer> ids = new HashMap<>();
+
+    static {
+        for (int i = 0; i < tagNames.size(); i++) {
+            ids.put(tagNames.get(i), i);
+        }
     }
 
-    public int getTagId() {
-        return tagId;
+    static public String getTagName(int index) {
+        return tagNames.get(index);
     }
 
-    public String getTagName() {
-        return tagName;
+    static public int getTagId(String tagName) {
+        return ids.get(tagName);
+    }
+
+    static public List<String> getAllTags() {
+        return tagNames;
+    }
+
+    static public List<Integer> tagsToTagIds(String tags) {
+        List<Integer> tagIds = new ArrayList<>();
+        for (int i = 0; i < tags.length(); i++) {
+            if (tags.charAt(i) != '0') {
+                tagIds.add(i);
+            }
+        }
+        return tagIds;
+    }
+
+    static public String tagNamesToTags(List<String> tagNames) {
+        Set<Integer> tagIds = new HashSet<>();
+        for (String tagName : tagNames) {
+            tagIds.add(getTagId(tagName));
+        }
+        StringBuilder tags = new StringBuilder();
+        for (int i = 0; i < tagNames.size(); i++) {
+            if (tagIds.contains(i)) {
+                tags.append('1');
+            } else {
+                tags.append('0');
+            }
+        }
+        return tags.toString();
     }
 
 }
