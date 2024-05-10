b2c87bf8e10533822405b0b4422b89e989c9a759
FirePancake1177 <poiskov2016@gmail.com>
2024-05-10 18:23:54 +0300

    Add authentication via JWT Token + minor fixes

---
+++ server/src/main/java/com/example/server/service/AuthenticationService.java
@@ -0,0 +1,61 @@
+package com.example.server.service;
+
+import com.example.server.dto.JwtAuthenticationResponse;
+import com.example.server.dto.UserLoginDto;
+import com.example.server.dto.UserRegistrationDto;
+import com.example.server.model.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.example.server.model.User;
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     * Регистрация пользователя
+     *
+     * @param request данные пользователя
+     * @return токен
+     */
+    public JwtAuthenticationResponse signUp(UserRegistrationDto request) {
+
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
+
+        userService.create(user);
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    /**
+     * Аутентификация пользователя
+     *
+     * @param request данные пользователя
+     * @return токен
+     */
+    public JwtAuthenticationResponse signIn(UserLoginDto request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+        ));
+
+        var user = userService
+                .userDetailsService()
+                .loadUserByUsername(request.getUsername());
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
