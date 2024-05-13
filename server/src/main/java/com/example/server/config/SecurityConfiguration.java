ab7cd3f10702bc193316f7397a3f40de9750a6cd
m4xxx1m <mi4469228@yandex.ru>
2024-05-13 23:28:47 +0300

    Some fixes in authorization system

--- server/src/main/java/com/example/server/config/SecurityConfiguration.java
+++ server/src/main/java/com/example/server/config/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(request -> request
                         // Можно указать конкретный путь, * - 1 уровень вложенности, ** - любое количество уровней вложенности
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/getTags").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/endpoint", "/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
