d5dddb7f277f944b81b44bd9c90aa6da3209a040
m4xxx1m <mi4469228@yandex.ru>
2024-06-09 23:14:02 +0300

    Fixes on post pagination

--- server/src/main/java/com/example/server/model/Comment.java
+++ server/src/main/java/com/example/server/model/Comment.java
@@ -9,28 +9,29 @@ public class Comment extends ContentObj {
 
     private int postId;
 
-    private int replyToCommentId;
+    private Integer replyToCommentId;
 
     public Comment() {
         super();
     }
 
-    public Comment(int postId, int replyToCommentId) {
+    public Comment(int postId, Integer replyToCommentId) {
         super();
         this.postId = postId;
         this.replyToCommentId = replyToCommentId;
     }
 
-    public Comment(int authorId, String content, int postId, int replyToCommentId) {
+    public Comment(int authorId, String content, int postId, Integer replyToCommentId) {
         super(authorId, content);
         this.postId = postId;
         this.replyToCommentId = replyToCommentId;
     }
+
     public int getPostId() {
         return postId;
     }
 
-    public int getReplyToCommentId() {
+    public Integer getReplyToCommentId() {
         return replyToCommentId;
     }
 
