003a16b93410ee2fb75d940c0d3c6163d68e97af
m4xxx1m <mi4469228@yandex.ru>
2024-06-18 00:01:34 +0300

    fixed bugs with comments

--- client/composeApp/src/commonMain/kotlin/model/Tag.kt
+++ client/composeApp/src/commonMain/kotlin/model/Tag.kt
@@ -1,8 +1,10 @@
 package model
 
 
-import kotlinx.coroutines.delay
 import network.RetrofitClientPreAuth
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 data class Tag(val tagId: Int, val tagName: String) {
     companion object {
@@ -16,42 +18,40 @@ data class Tag(val tagId: Int, val tagName: String) {
         val defaultTags
             get() = defaultTags_!!
         
-        suspend fun initTags() {
-//            RetrofitClientPreAuth.retrofitCall.getTags().enqueue(object :
-//                Callback<ArrayList<String>> {
-//                override fun onResponse(
-//                    call: Call<ArrayList<String>>,
-//                    response: Response<ArrayList<String>>
-//                ) {
-//                    if (response.code() == 200) {
-//                        tags_ = response.body()
-//                        defaultTags_ = "0".repeat(tags.size)
-//                    } else {
-//                        println("wrong code on getting all tags")
-//                        initTags()
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<ArrayList<String>>, t: Throwable) {
-//                    println("failure on getting all tags")
-//                    initTags()
-//                }
-//
-//            })
-            while (true) {
-                try {
-                    val response = RetrofitClientPreAuth.retrofitCall.getTags().execute()
+        fun initTags() {
+            RetrofitClientPreAuth.retrofitCall.getTags().enqueue(object :
+                Callback<ArrayList<String>> {
+                override fun onResponse(
+                    call: Call<ArrayList<String>>,
+                    response: Response<ArrayList<String>>
+                ) {
                     if (response.code() == 200) {
                         tags_ = response.body()
                         defaultTags_ = "0".repeat(tags.size)
-                        break
+                    } else {
+                        initTags()
                     }
-                    println("wrong code on getting tags")
-                } catch (_: Throwable) {
-                    println("error on getting tags")
                 }
-                delay(1000)
-            }
+
+                override fun onFailure(call: Call<ArrayList<String>>, t: Throwable) {
+                    initTags()
+                }
+
+            })
+//            while (true) {
+//                try {
+//                    val response = RetrofitClientPreAuth.retrofitCall.getTags().execute()
+//                    if (response.code() == 200) {
+//                        tags_ = response.body()
+//                        defaultTags_ = "0".repeat(tags.size)
+//                        break
+//                    }
+//                    println("wrong code on getting tags")
+//                } catch (_: Throwable) {
+//                    println("error on getting tags")
+//                }
+//                delay(300)
+//            }
         }
     }
 }
