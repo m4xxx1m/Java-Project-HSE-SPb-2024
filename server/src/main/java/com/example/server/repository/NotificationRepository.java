75a7f342b6f76deaa4b0feb3ade4212ebb878e34
m4xxx1m <mi4469228@yandex.ru>
2024-06-17 20:53:14 +0300

    Changes and fixes in notifications

--- server/src/main/java/com/example/server/repository/NotificationRepository.java
+++ server/src/main/java/com/example/server/repository/NotificationRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     List<Notification> findByOriginalCommentAuthorId(int originalCommentAuthorId);
+
+    List<Notification> findByOriginalCommentAuthorIdOrderByReplyCommentIdDesc(int originalCommentAuthorId);
 }
